67120e60edf493d7965f6c8de954f4b981168d27
OmarMayta <i1310579@continental.edu.pe>
2025-03-02 05:17:45 -0500

    actualizando

--- ses3.py
+++ ses3.py
@@ -1,22 +1,22 @@
 import streamlit as st
+
 # Función clasificar el puntaje
 def clasificar_puntaje(puntaje):
-    if puntaje >=85:
+    if puntaje >= 85:
         return "Excelente"
-    elif puntaje >=70:
+    elif puntaje >= 70:
         return "Bueno"
     else:
         return "Necesita Mejorar"
 
-#interfaz en streamlit
-st.title("Clasificacion de puntajes")
-st.write("Ingrese puntaje y el sistema lo clasificará")
+# Interfaz en Streamlit
+st.title("Clasificación de Puntajes")
+st.write("Ingrese un puntaje y el sistema lo clasificará")
 
-#entrada de usuario
-puntaje = st.number_input("Ingrese un puntaje (0-100):", min_value=0, max value=100, step=1)
+# Entrada de usuario
+puntaje = st.number_input("Ingrese un puntaje (0-100):", min_value=0, max_value=100, step=1)
 
-#boton clasificar
-    if st.button("Clasificar"):
-        resultado = clasificar_puntaje(puntaje)
-        st.success(f"El puntaje es clasificado como: {resultado}")
-
+# Botón clasificar
+if st.button("Clasificar"):
+    resultado = clasificar_puntaje(puntaje)
+    st.success(f"El puntaje es clasificado como: {resultado}")
